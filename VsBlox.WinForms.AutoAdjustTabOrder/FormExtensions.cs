11f2b29487517009ba1a4787ebf06d6dee2b02a5
Sybren <Sybren@SYBREN-ACER>
2016-06-28 08:35:28 +0200

    Add project files.

---
+++ VsBlox.WinForms.AutoAdjustTabOrder/FormExtensions.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace VsBlox.WinForms.AutoAdjustTabOrder
+{
+  /// <summary>
+  /// Windows Forms extension methods.
+  /// </summary>
+  public static class FormExtensions
+  {
+    /// <summary>
+    /// Adjusts the tab order of the controls of a Windows Form.
+    /// It operates like this:
+    /// 
+    /// Collect all controls and sort on the Y coordinate of the mid of the control..
+    /// Process controls in rows. All control with the same Y coordinate (+/- threshold)
+    /// belong to the same row. Because all controls are sorted on their Y coordinate,
+    /// only the Y coordinate of the previous control is significant in the row ordering.
+    /// Finally all controls in the same row are sorted on their X coordinate.
+    /// The tab order is from top to bottom, from left to right. Processing is recursive. 
+    /// </summary>
+    /// <param name="form">The windows form.</param>
+    /// <example>
+    /// Usage:
+    /// <code>
+    /// public MyForm()
+    /// {
+    ///     ...
+    ///     Load += (sender, e) =>
+    ///     {
+    ///         ...
+    ///         this.AdjustTabOrder();
+    ///         ...
+    ///     }
+    ///     ...
+    /// };
+    /// </code>
+    /// </example>
+    public static void AdjustTabOrder(this Form form)
+    {
+      if (form.Controls.Count > 0) AdjustTabOrderHelper(form);
+    }
+
+    /// <summary>
+    /// Helper function to reorder the controls tab order.
+    /// </summary>
+    /// <param name="rootControl">The root control.</param>
+    private static void AdjustTabOrderHelper(Control rootControl)
+    {
+      // Verzamel alle control en sorteer op Y.
+      var controls = new List<Control>();
+
+      foreach (Control childControl in rootControl.Controls)
+      {
+        if (childControl.Controls.Count > 0) AdjustTabOrderHelper(childControl);
+
+        if (childControl is TabPage) continue;
+
+        controls.Add(childControl);
+      }
+
+      const int bandWidthY = 10;
+
+      // Sorteer op middelpunt van de hoogte van de control en
+      // maak rijen waar de controls met ongeveer dezelfde y waarde inzitten.
+      var rows = new List<List<Control>>();
+      var lastY = -bandWidthY;
+
+      foreach (var control in controls.OrderBy(c => c.Location.Y))
+      {
+        var currentY = control.Location.Y;
+
+        // Als de afwijking t.o.v. de vorige groter of gelijk is aan de bandbreedte, dan starten we een nieuwe rij.
+        if (Math.Abs(currentY - lastY) >= bandWidthY || rows.Count == 0) rows.Add(new List<Control>());
+
+        // Voeg control toe aan een rij.
+        rows[rows.Count - 1].Add(control);
+
+        lastY = currentY;
+      }
+
+      // Doorloop de rijen.
+      // Per rij: sorteer op X
+      // Per X: zet de tab volgorde.
+      var tabIndex = 0;
+
+      foreach (var control in rows.Select(row => row.OrderBy(c => c.Location.X)).SelectMany(rowControls => rowControls))
+      {
+        if ((control is RichTextBox && !control.Enabled) || control is Label)
+        {
+          control.TabIndex = 0;
+          control.TabStop = false;
+          continue;
+        }
+
+        control.TabIndex = tabIndex++;
+      }
+    }
+  }
+}
---
+++ VsBlox.WinForms.AutoAdjustTabOrder/NamespaceDoc.cs
@@ -0,0 +1,10 @@
+﻿namespace VsBlox.WinForms.AutoAdjustTabOrder
+{
+  /// <summary>
+  /// Adjusts the tab order of the controls of a Windows Form.
+  /// </summary>
+  [System.Runtime.CompilerServices.CompilerGenerated()]
+  class NamespaceDoc
+  {
+  }
+}
